58d42f754af68d8b18a845308f9e31c994edf449
An-172N <2563744997@qq.com>
2025-09-10 23:50:36 +0800

    Delete SRCS directory

--- SRCS/TOOLS/DISTANCE.py
+++
@@ -1,7 +0,0 @@
-import math
-
-def dist(tgt, src):
-    dx = tgt.rect.centerx - src.rect.centerx
-    dy = tgt.rect.centery - src.rect.centery
-
-    return math.hypot(dx, dy)
--- SRCS/TOOLS/FRICTION.py
+++
@@ -1,8 +0,0 @@
-def fric(spr, fric):
-    spr.x = getattr(spr, 'x', spr.rect.x)
-
-    spr.vel_x *= fric
-
-    spr.x -= spr.vel_x
-
-    spr.rect.x = int(spr.x)
