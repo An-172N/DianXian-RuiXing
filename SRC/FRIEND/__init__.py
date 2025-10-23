69177e1baa1503d0cacc1ae956ef27a8560c692e
An-172N <2563744997@qq.com>
2025-10-23 15:03:01 +0800

    重构SRC

--- SRC/FRIEND/__init__.py
+++
@@ -1,5 +0,0 @@
-from FRIEND.DIVIDE.AUTFRODIFFUSE import AutFroDiffuse
-from FRIEND.DIVIDE.RECTRAINING import RectRaining
-
-from FRIEND.HUMAN.KLI import Kli
-from FRIEND.HUMAN.ONO import Ono
--- SRC/LOGIC/__init__.py
+++
@@ -1,8 +0,0 @@
-from LOGIC.SPRITE.PLANE import PlaneMgr
-from LOGIC.SPRITE.BULLET import BulletMgr
-from LOGIC.SPRITE.ITEM import ItemMgr
-from LOGIC.SPRITE.WHITEBRICK import WhiteBrick
-
-from LOGIC.PROCESS.STAGE import StageMgr
-from LOGIC.PROCESS.GUI import GUI
-from LOGIC.PROCESS.KEY import Key
