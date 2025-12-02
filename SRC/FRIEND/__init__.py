486bc9f98a7b6e031bb4e8b64dc38e1d2100a5f0
An-172N <2563744997@qq.com>
2025-12-02 20:51:34 +0800

    整理一下

--- SRC/FRIEND/__init__.py
+++
@@ -1,7 +0,0 @@
-from FRIEND.HUMAN.KLI import Kli
-from FRIEND.HUMAN.ONO import Ono
-from FRIEND.HUMAN.HRO import Hro
-from FRIEND.HUMAN.NRE import Nre
-
-from FRIEND.BASE import Base
-from FRIEND.HUMAN.KLI import DecPt
