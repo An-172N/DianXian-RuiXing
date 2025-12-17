2ced53bf34d743097588c34e86fe2fabd4efe3bf
An-172N <2563744997@qq.com>
2025-12-17 16:19:16 +0800

    更新一下
    没了

--- PACK.bat
+++ PACK.bat
@@ -6,8 +6,8 @@ nuitka ^
     --lto=yes ^
     --windows-console-mode=disable ^
     --enable-plugin=anti-bloat ^
-    --file-version="0.7" ^
-    --product-version="0.7" ^
+    --file-version="0.7.1" ^
+    --product-version="0.7.1" ^
     --product-name="RuiXing ~ Thunder Out of the Mountain" ^
     --copyright="Copyright (c) 2025 An_172N" ^
     --windows-icon-from-ico=ASSET\IMG_ICON.png ^
@@ -16,5 +16,5 @@ nuitka ^
     --nofollow-import-to=tkinter,unittest,test,pdb,doctest,setuptools,pip,email,http,urllib,xml,sqlite3,multiprocessing,html,distutils,cgi,cgitb,wsgiref,smtplib,poplib,imaplib,idlelib,pydoc,gettext,locale,threading,ordered_set,venv,ensurepip,ftplib,wave,audioop ^
     --nofollow-import-to=pygame.mixer,pygame.mixer_music,pygame.music,pygame.newbuffer,pygame.pypm,pygame.gfxdraw,pygame.cdrom,pygame.joystick,pygame.mouse,pygame.movie,pygame.overlay,pygame.sndarray,pygame.surfarray,pygame.scrap,pygame.camera,pygame.macosx,pygame.midi,pygame.threads,pygame.examples ^
     --nofollow-import-to=pygame._sdl2,pygame._camera_opencv,pygame._camera_vidcapture,pygame.__pyinstaller,pygame._camera ^
-    --nofollow-import-to=numpy,imageio,pillow,nuitka,zstandard ^
+    --nofollow-import-to=numpy ^
     .\MAIN.py
