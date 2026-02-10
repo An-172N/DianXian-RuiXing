15c6b8e68306f8e8038ccfa08ad797394d44b5f7
An-172N <2563744997@qq.com>
2026-02-10 19:20:58 +0800

    删除SOURCE
    整理一下

--- SOURCE/HUMAN/__init__.py
+++
@@ -1,9 +0,0 @@
-# (C)opyright 2026 An_172N
-# 此代码根据 GPLv3.0 许可证授权
-
-
-from SOURCE.HUMAN.ONO import Ono
-from SOURCE.HUMAN.HRO import Hro
-from SOURCE.HUMAN.NRE import Nre
-from SOURCE.HUMAN.QDI import Qdi
-from SOURCE.HUMAN.KLI import Kli
