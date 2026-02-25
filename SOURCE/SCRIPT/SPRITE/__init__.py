74048e851d6c49449fc30996491d19f2b0d5c27b
An-172N <2563744997@qq.com>
2026-02-25 21:08:12 +0800

    整理一下SOURCE
    然后上传~

--- SOURCE/SCRIPT/SPRITE/__init__.py
+++
@@ -1,9 +0,0 @@
-# (C)opyright 2026 An_172N
-# 此代码遵循 GPLv3.0 协议
-
-
-import SCRIPT.SPRITE.BARRAGE as Barrage
-import SCRIPT.SPRITE.BRICK as Brick
-import SCRIPT.SPRITE.BULLET as Bullet
-import SCRIPT.SPRITE.ITEM as Item
-import SCRIPT.SPRITE.LINE as Line
