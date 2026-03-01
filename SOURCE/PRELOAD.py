a864a156360662163f7bdf1c8463c9ce4a819bd4
An-172N <2563744997@qq.com>
2026-03-01 20:09:39 +0800

    更新一下
    微调了一下音效的音量

--- SOURCE/PRELOAD.py
+++ SOURCE/PRELOAD.py
@@ -36,7 +36,7 @@ icon = pg.display.set_icon(pg.image.load(BytesIO(asset(r'ASSET\IMAGE\ICON.png'))
 
 
 sound_cache = {
-    i: (sound := pg.mixer.Sound(BytesIO(asset(rf'ASSET\OGG\{i.upper()}.oga'))), sound.set_volume(j))[0] for i, j in (('pick', 0.7), ('fire', 1.0), ('charge', 0.95))
+    i: (sound := pg.mixer.Sound(BytesIO(asset(rf'ASSET\OGG\{i.upper()}.oga'))), sound.set_volume(j))[0] for i, j in (('pick', 0.72), ('fire', 1.0), ('charge', 0.9))
 }
 
 
