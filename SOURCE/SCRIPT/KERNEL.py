07feb13d6fbbb31faf5651384eefe50410697d05
An-172N <2563744997@qq.com>
2026-03-10 14:54:09 +0800

    更新一下
    刚刚才发现Clear这个单词拼错了，现在已经改回来了
    然后没了

--- SOURCE/SCRIPT/KERNEL.py
+++ SOURCE/SCRIPT/KERNEL.py
@@ -118,7 +118,7 @@ def talk(screen: pg.Surface):
 
 
 def summary(screen: pg.Surface):
-    stage = f"Stage {GLOBAL.stage if GLOBAL.stage <= 3 else 'Extra'} - {GLOBAL.level} Cleaer! {'Hit Z Key.' if GLOBAL.level <= 5 else ''}"
+    stage = f"Stage {GLOBAL.stage if GLOBAL.stage <= 3 else 'Extra'} - {GLOBAL.level} Clear! {'Hit Z Key.' if GLOBAL.level <= 5 else ''}"
     text = [
         f"得点 {GLOBAL.total_point} * 512 = {GLOBAL.total_point * 512}",
         f"无闪 {GLOBAL.unflash} * 4096 = {GLOBAL.unflash * 4096}"
