a40c5450d9fabed6e5fe86ce98a1f0268aa2067b
An-172N <2563744997@qq.com>
2026-05-29 17:14:52 +0800

    SOURCE/LOGIC/__init__.py貌似没用

--- SOURCE/LOGIC/__init__.py
+++
@@ -1,9 +0,0 @@
-# (C)opyright 2026 An_172N
-# 此代码遵循 GPLv3.0 协议
-
-
-import LOGIC.CALCULATE as Calculate
-import LOGIC.FILE as File
-import LOGIC.GRAPHIC as Graphic
-import LOGIC.SPRITE as Sprite
-import LOGIC.STAGE as Stage
