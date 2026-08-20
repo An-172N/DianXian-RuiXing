08bdcef9d51473f771e40f8d0f8973ca68bb53a2
An-172N <2563744997@qq.com>
2026-08-20 17:06:40 +0800

    更新一下
    还是返璞归真吧……

--- __main__.py
+++ __main__.py
@@ -23,7 +23,6 @@ def read_resource(file):
 def main():
     title = '锐行 ~ Thunder Out of the Mountain'
     version = '1.2.3'
-    author = 'An_172N'
     sys.dont_write_bytecode = True
     sys.modules['numpy'] = None
     parser = argparse.ArgumentParser()
@@ -31,40 +30,20 @@ def main():
         parser.add_argument(i, type=int, default=j)
     args = parser.parse_args()
     random.seed(args.sd)
-    while True:
-        choose = input(
-            f"={title}==================\n"
-            "TH：去锐山\n"
-            "R ：读我\n"
-            "L ：程序代码许可\n"
-            "Q ：回家吃烤鸡扒\n"
-            f"==============================Ver {version} | By {author}=\n"
-        ).upper()
-        if choose == "TH":
-            import pygame
 
-            clock = pygame.time.Clock()
-            pygame.display.init()
-            pygame.font.init()
-            pygame.mixer.init()
-            pygame.mixer.set_num_channels(2)
+    import pygame
 
-            import KERNEL
+    clock = pygame.time.Clock()
+    pygame.display.init()
+    pygame.font.init()
+    pygame.mixer.init()
+    pygame.mixer.set_num_channels(2)
 
-            pygame.display.set_caption(title)
-            KERNEL.update(clock, (int(args.s), int(args.l), int(args.f), int(args.p)), version, title)
-            os.execv(sys.executable, ['python'] + sys.argv)
-        elif choose == "R":
-            content = read_resource("README.md")
-            print(content)
-        elif choose == "L":
-            content = read_resource("LICENSE")
-            print(content)
-        elif choose == "Q":
-            print("还是奥尔良鸡扒？")
-            break
-        else:
-            print("这是什么呀？")
+    import KERNEL
+
+    pygame.display.set_caption(title)
+    print(read_resource("README.md"))
+    KERNEL.update(clock, (int(args.s), int(args.l), int(args.f), int(args.p)), version, title)
 
 
 if __name__ == "__main__":
